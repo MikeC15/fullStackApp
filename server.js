db2a5259ba1e994ab4bc18781f85c04e9bb6465a
Mike Cardarelli <Mike@Mikes-MacBook-Air.local>
2019-10-20 09:31:45 -0600

    server setup complete

--- server.js
+++ server.js
@@ -2,6 +2,14 @@
 
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const showsController = require("./controllers/shows");
+require("./db/db")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use("/shows", showsController)
 
 
 
