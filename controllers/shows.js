8fb7744036fc5b444881c40568e87ec74e41e266
Mike Cardarelli <Mike@Mikes-MacBook-Air.local>
2019-10-20 10:12:02 -0600

    new post route grabbing reqbody

--- controllers/shows.js
+++ controllers/shows.js
@@ -1,12 +1,30 @@
 const express = require("express")
 const router = express.Router()
-const show = require("../models/shows")
+const Show = require("../models/shows")
 
 //routes
 
 //new route
 router.get("/new", (req, res)=>{
     res.render('new.ejs')
+})
+
+//post route
+router.post("/", (req, res)=>{
+    console.log(req.body)
+})
+
+//index route
+router.get("/", (req, res)=>{
+    Show.find({}, (err, foundShows)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.render("index.ejs", {
+                shows: foundShows
+            })
+        }
+    })
 })
 
 module.exports = router;
